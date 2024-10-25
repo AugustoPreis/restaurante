c18e72846be979f31bba2743fe4633848709e17d
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-10-25 11:38:59 -0300

    adiciona opcao de buscar forma de pagamento por id

--- backend/src/controllers/formaPagamento/formaPagamentoRepository.ts
+++ backend/src/controllers/formaPagamento/formaPagamentoRepository.ts
@@ -4,6 +4,14 @@ import { FormaPagamento } from '../../models/FormaPagamento';
 export class FormaPagamentoRepository {
   private readonly repository = Database.getRepository(FormaPagamento);
 
+  async buscarPorId(id: number): Promise<FormaPagamento> {
+    return await this.repository
+      .createQueryBuilder('forpag')
+      .where('forpag.id = :id', { id })
+      .andWhere('forpag.ativo IS TRUE')
+      .getOne();
+  }
+
   async listar(filtro: string): Promise<[FormaPagamento[], number]> {
     return await this.repository
       .createQueryBuilder('forpag')
--- backend/src/controllers/formaPagamento/formaPagamentoService.ts
+++ backend/src/controllers/formaPagamento/formaPagamentoService.ts
@@ -1,6 +1,10 @@
 import { formaPagamentoRepository } from '.';
+import { HttpStatusCode } from '../../enums/HttpStatusCode';
 import { defaultParams } from '../../utils/params';
+import { RequestError } from '../../utils/RequestError';
+import { isValidNumber } from '../../utils/validators';
 import { UsuarioLogadoDTO } from '../usuario/dtos/UsuarioLogadoDTO';
+import { FormaPagamentoConsultaDTO } from './dtos/FormaPagamentoConsultaDTO';
 import { FormaPagamentoListagemDTO } from './dtos/FormaPagamentoListagemDTO';
 import { FormaPagamentoListagemParametrosDTO } from './dtos/FormaPagamentoListagemParametrosDTO';
 import { FormaPagamentoListagemResultadoDTO } from './dtos/FormaPagamentoListagemRetorno';
@@ -25,5 +29,23 @@ export class FormaPagamentoService {
     }
 
     return { data: formasPagamentoListagemDTO, total };
+  }
+
+  async buscarPorId(id: number): Promise<FormaPagamentoConsultaDTO> {
+    if (!isValidNumber(id, { min: 1 })) {
+      throw new RequestError(HttpStatusCode.BAD_REQUEST, 'ID da forma de pagamento inválido');
+    }
+
+    const formaPagamentoModel = await formaPagamentoRepository.buscarPorId(id);
+
+    if (!formaPagamentoModel) {
+      throw new RequestError(HttpStatusCode.NOT_FOUND, 'Forma de pagamento não encontrada');
+    }
+
+    const formaPagamentoConsultaDTO: FormaPagamentoConsultaDTO = {};
+
+    formaPagamentoConsultaDTO.descricao = formaPagamentoModel.descricao;
+
+    return formaPagamentoConsultaDTO;
   }
 }
