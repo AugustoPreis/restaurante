e1445418a10f92cf70d3fe467cdf0c0882dad678
Augusto <augustopreisthomasi@gmail.com>
2024-11-23 13:09:46 -0300

    remove react-icons

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import { IntlProvider } from 'react-intl';
-import { IconContext } from 'react-icons';
 import ptBR from 'antd/lib/locale/pt_BR';
 import Suspense from './components/Suspense';
 import AuthProvider from './providers/AuthProvider';
@@ -16,18 +15,13 @@ appRoot.render(
   <Suspense>
     <ConfigProvider locale={ptBR}>
       <IntlProvider locale='pt'>
-        <IconContext.Provider value={{
-          className: 'anticon',
-          style: { verticalAlign: 'middle', fontSize: 15 },
-        }}>
-          <Suspense>
-            <BrowserRouter>
-              <AuthProvider>
-                <AppRouter />
-              </AuthProvider>
-            </BrowserRouter>
-          </Suspense>
-        </IconContext.Provider>
+        <Suspense>
+          <BrowserRouter>
+            <AuthProvider>
+              <AppRouter />
+            </AuthProvider>
+          </BrowserRouter>
+        </Suspense>
       </IntlProvider>
     </ConfigProvider>
   </Suspense>
