b72c07971ed31532c1ef94730300742247b648a4
Augusto <augustopreisthomasi@gmail.com>
2024-11-23 13:13:29 -0300

    remove react-intl

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,6 @@ import React, { lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
-import { IntlProvider } from 'react-intl';
 import ptBR from 'antd/lib/locale/pt_BR';
 import Suspense from './components/Suspense';
 import AuthProvider from './providers/AuthProvider';
@@ -14,15 +13,13 @@ const appRoot = createRoot(document.getElementById('root'));
 appRoot.render(
   <Suspense>
     <ConfigProvider locale={ptBR}>
-      <IntlProvider locale='pt'>
-        <Suspense>
-          <BrowserRouter>
-            <AuthProvider>
-              <AppRouter />
-            </AuthProvider>
-          </BrowserRouter>
-        </Suspense>
-      </IntlProvider>
+      <Suspense>
+        <BrowserRouter>
+          <AuthProvider>
+            <AppRouter />
+          </AuthProvider>
+        </BrowserRouter>
+      </Suspense>
     </ConfigProvider>
   </Suspense>
 );
