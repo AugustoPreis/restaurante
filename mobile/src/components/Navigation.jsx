b00d0342726a9c90e942cedb249103da01eae0e6
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-12-10 16:10:28 -0300

    adiciona tela de pedidos vazia

--- mobile/src/components/Navigation.jsx
+++ mobile/src/components/Navigation.jsx
@@ -3,12 +3,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //Screens
 import Login from '../screens/login/Login';
+import Pedidos from '../screens/pedidos/Pedidos';
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Login',
+  screenOptions: {
+    headerBackVisible: false,
+  },
   screens: {
     Login: {
       screen: Login,
+    },
+    Pedidos: {
+      screen: Pedidos,
     },
   },
 });
