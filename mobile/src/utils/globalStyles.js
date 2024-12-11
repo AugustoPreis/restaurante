09e72d25bd1e33c5e0c029ce072f7f1ff2737faf
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-12-11 11:54:56 -0300

    cria global styles

---
+++ mobile/src/utils/globalStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  marginBottom: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#00000030',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
